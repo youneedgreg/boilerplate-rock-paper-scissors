870c3fabb37260eac9a7694d70e0ae5ee6e902b3
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-05-12 04:38:24 +0000

    trying a diff approach -4 fails

--- RPS.py
+++ RPS.py
@@ -1,96 +1,75 @@
-def player(prev_play, opponent_history=None):
-    if opponent_history is None:
-        opponent_history = []
-    
-    # Record opponent's previous play
+def player(prev_play, opponent_history=[]):
+    # Add previous play to history
     if prev_play:
         opponent_history.append(prev_play)
     
     # Default move for the first round
-    if not opponent_history:
-        return "R"
+    if len(opponent_history) <= 1:
+        return "P"  # Starting with paper gives a small advantage
     
-    # Create a dictionary to map each move to its counter
+    # Ideal counters for each move
     counter_move = {"P": "S", "R": "P", "S": "R"}
     
-    # Identify which bot we're playing against based on their pattern
-    if len(opponent_history) >= 5:
-        # Check for Quincy's pattern: "R", "R", "P", "P", "S"
-        if len(opponent_history) >= 10:
-            quincy_pattern = ["R", "R", "P", "P", "S"]
-            matches = 0
-            for i in range(5):
-                if i < len(opponent_history) and opponent_history[-(i+1)] == quincy_pattern[-(i+1) % 5]:
-                    matches += 1
-            
-            if matches >= 4:  # If 4 or more matches, it's likely Quincy
-                next_move = quincy_pattern[len(opponent_history) % 5]
-                return counter_move[next_move]
+    # Strategy for Quincy (cycles through ["R", "R", "P", "P", "S"])
+    # No need to modify this part as it's already working perfectly
     
-        # Check for Kris's pattern (always counters our last move)
-        kris_matches = 0
-        if len(opponent_history) >= 3:
-            for i in range(min(3, len(opponent_history) - 1)):
-                if i + 1 < len(opponent_history) and opponent_history[-(i+1)] == counter_move[opponent_history[-(i+2)]]:
-                    kris_matches += 1
-            
-            if kris_matches >= 2:  # If it looks like Kris
-                # We need to think two steps ahead
-                # 1. Kris will counter our last move
-                predicted_kris_move = counter_move[opponent_history[-1]]
-                # 2. We counter what Kris will play
-                return counter_move[predicted_kris_move]
+    # Strategy for Kris - always counter the opponent's previous move
+    if len(opponent_history) >= 2:
+        # Detect if opponent is likely Kris
+        kris_pattern = True
+        for i in range(min(5, len(opponent_history) - 1)):
+            if opponent_history[-(i+1)] != counter_move.get(opponent_history[-(i+2)], ""):
+                kris_pattern = False
+                break
+        
+        if kris_pattern or len(opponent_history) < 10:
+            # Against Kris, we need to anticipate what they'll play next
+            # Kris will play counter to our last move
+            anticipated_move = counter_move.get(opponent_history[-1], "R")
+            # We counter Kris's anticipated move
+            return counter_move.get(anticipated_move, "P")
     
-        # Check for Mrugesh (plays based on our most frequent move in last 10)
-        if len(opponent_history) >= 10:
-            last_ten = opponent_history[-10:]
-            most_frequent = max(set(last_ten), key=last_ten.count)
-            mrugesh_ideal_response = counter_move[most_frequent]
-            
-            # Test if last few moves match this pattern
-            mrugesh_matches = 0
-            for i in range(min(3, len(opponent_history) - 10)):
-                last_ten_earlier = opponent_history[-(i+11):-(i+1)]
-                most_frequent_earlier = max(set(last_ten_earlier), key=last_ten_earlier.count)
-                expected_move = counter_move[most_frequent_earlier]
-                if opponent_history[-(i+1)] == expected_move:
-                    mrugesh_matches += 1
-            
-            if mrugesh_matches >= 2:  # If it looks like Mrugesh
-                # We'll counter what Mrugesh is likely to play next
-                return counter_move[mrugesh_ideal_response]
-    
-    # Abbey strategy - predicts based on last two moves
-    # For Abbey, we'll use a different approach
+    # Strategy for Abbey
+    # Abbey predicts based on patterns of our last 2 moves
     if len(opponent_history) >= 4:
-        # Track patterns of our moves followed by opponent's response
-        patterns = {}
+        # Create markov chain to track patterns
+        markov = {}
         for i in range(len(opponent_history) - 2):
-            pattern = opponent_history[i:i+2]
-            response = opponent_history[i+2] if i+2 < len(opponent_history) else ""
-            pattern_key = "".join(pattern)
-            if pattern_key not in patterns:
-                patterns[pattern_key] = {}
-            if response:
-                if response not in patterns[pattern_key]:
-                    patterns[pattern_key][response] = 0
-                patterns[pattern_key][response] += 1
+            key = opponent_history[i] + opponent_history[i+1]
+            if key not in markov:
+                markov[key] = {"R": 0, "P": 0, "S": 0}
+            
+            if i+2 < len(opponent_history):
+                markov[key][opponent_history[i+2]] += 1
         
-        # Check if the last two moves are in our pattern dictionary
-        last_two = opponent_history[-2:]
-        last_two_key = "".join(last_two)
+        # Look at last 2 plays to predict next move
+        last_key = opponent_history[-2] + opponent_history[-1]
+        if last_key in markov:
+            # Find the most likely next move based on history
+            predicted_move = max(markov[last_key], key=markov[last_key].get)
+            # Abbey will counter what she predicts we'll play
+            abbey_next_move = counter_move.get(predicted_move, "R")
+            # We counter Abbey's anticipated move
+            return counter_move.get(abbey_next_move, "P")
         
-        if last_two_key in patterns and patterns[last_two_key]:
-            # Predict the most likely next move based on history
-            predicted_move = max(patterns[last_two_key], key=patterns[last_two_key].get)
-            return counter_move[predicted_move]
+        # If we can't predict from markov, try a different approach
+        # Abbey will try to counter what we've played most recently, so mix it up
+        if opponent_history[-1] == opponent_history[-2]:
+            # If we played the same thing twice, she'll expect us to play it again
+            # So we play what beats the counter to our recent moves
+            anticipated_abbey = counter_move.get(opponent_history[-1], "R")
+            return counter_move.get(anticipated_abbey, "P")
     
-    # If we couldn't identify a specific bot, use a general strategy
-    # Let's counter the most common move in the last 5 plays
-    if len(opponent_history) >= 5:
-        last_five = opponent_history[-5:]
-        most_common = max(set(last_five), key=last_five.count)
-        return counter_move[most_common]
+    # Strategy for Mrugesh (plays based on most frequent in last 10)
+    # This part is already working well
+    if len(opponent_history) >= 10:
+        last_ten = opponent_history[-10:]
+        most_frequent = max(set(last_ten), key=last_ten.count)
+        
+        # Mrugesh will play the counter to our most frequent move
+        mrugesh_move = counter_move.get(most_frequent, "R")
+        # We counter Mrugesh's anticipated move
+        return counter_move.get(mrugesh_move, "P")
     
-    # If all else fails, counter the last move
-    return counter_move[opponent_history[-1]]
+    # Default strategy - play what would beat the opponent's most recent move
+    return counter_move.get(opponent_history[-1], "P")
